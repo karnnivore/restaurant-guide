e31ededf6d8b5e6aac280a3e41f89fd5ea18165d
David Mosquera <davidmosq95@gmail.com>
2021-03-28 14:12:33 -0400

    About and splash changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+   // private platform: Platform,
+    //public router: Router
+  ) {
+   // this.initializeApp();
+  }
+
+  //initializeApp() {
+  //  this.platform.ready().then(() => {
+   //   this.router.navigateByUrl('splash');
+   // })
+ // }
 }
