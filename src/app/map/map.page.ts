c69c5aeff12228a4ccfd3df1b2bbeaa9805445d6
Nick Chinsen <nicholaschinsen@gmail.com>
2021-03-31 21:07:46 -0400

    Create review page layout

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  //data passed from prev modal
+  @Input() name: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //dismiss modal
+  dismiss() {
+    this.modalController.dismiss({
+      'dismissed': true
+    })
   }
 
 }
--- src/app/review/review.page.ts
+++ src/app/review/review.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-review',
   templateUrl: './review.page.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewPage implements OnInit {
 
-  constructor() { }
+  //data passed from prev modal
+  @Input() name: string;
+
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  //dismiss modal
+  dismiss() {
+    this.modalController.dismiss({
+      'dismissed': true
+    })
   }
 
 }
