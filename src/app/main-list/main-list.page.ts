ee8c47c26a365e9178b9f94bca7c2e80c544b26d
Nick Chinsen <nicholaschinsen@gmail.com>
2021-03-31 20:51:01 -0400

    Added modals for each restaurant

--- src/app/main-list/main-list.page.ts
+++ src/app/main-list/main-list.page.ts
@@ -21,9 +21,54 @@ export class MainListPage implements OnInit {
       componentProps: {
         'name': 'Tony\'s Pizza',
         'address': '123 Fake St.',
-        'rating' : 5,
+        'rating' : 4,
         'description' : 'The best pizza in town',
         'tags' : ['Italian', 'Pizza']
+      }
+    })
+    return await modal.present()
+  }
+
+  async presentRajmahal() {
+    const modal = await this.modalController.create({
+      component: DetailsModalPage,
+      cssClass: 'customModal',
+      componentProps: {
+        'name': 'Rajmahal',
+        'address': '241 Yonge St.',
+        'rating' : 4,
+        'description' : 'Really great indian food',
+        'tags' : ['Indian', 'Roti']
+      }
+    })
+    return await modal.present()
+  }
+
+  async presentThai() {
+    const modal = await this.modalController.create({
+      component: DetailsModalPage,
+      cssClass: 'customModal',
+      componentProps: {
+        'name': 'Thai Palace',
+        'address': '573 College St.',
+        'rating' : 4,
+        'description' : 'The best Pad Thai',
+        'tags' : ['Thai', 'Pad Thai']
+      }
+    })
+    return await modal.present()
+  }
+
+  async presentChez() {
+    const modal = await this.modalController.create({
+      component: DetailsModalPage,
+      cssClass: 'customModal',
+      componentProps: {
+        'name': 'Chez Paris',
+        'address': '273 Church St.',
+        'rating' : 4,
+        'description' : 'Bon apetit, great french food',
+        'tags' : ['French', 'Gourmet']
       }
     })
     return await modal.present()
