dfff72966eb0ca59b4181443f9b29c51b6bcde4a
Nick Chinsen <nicholaschinsen@gmail.com>
2021-04-11 22:21:57 -0400

    add markers to map

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -19,13 +19,59 @@ export class MapPage implements OnInit {
 
   @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
 
+  //marker code
+  infoWindows: any = [];
+  markers: any = {
+  };
+
   constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+    this.markers = {
+      title: this.name,
+      latitude: -17.824858,
+      longitude: 31.053028
+    }
   }
 
   ionViewDidEnter() {
     this.showMap();
+  }
+
+  addMarkersToMap(markers) {
+      let position = new google.maps.LatLng(markers.latitude, markers.longitude)
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: markers.title,
+        latitude: markers.latitude,
+        longitude: markers.longitude
+      })
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker)
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div>' + 
+                            '<h2 id="firstHeading" class="firstHeading content" style="color: black">' + marker.title + '</h2>' +
+                            '<p class="content" style="color: black">Latitude: ' + marker.latitude + '</p>' + 
+                            '<p class="content" style="color: black">Longitude: ' + marker.longitude + '</p>' +
+                            '</div>';
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    })
+
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows()
+      infoWindow.open(this.map, marker);
+    })
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for (let window of this.infoWindows) {
+      window.close()
+    }
   }
 
   //display new map
@@ -37,6 +83,7 @@ export class MapPage implements OnInit {
       disableDefaultUI: true
     }
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers)
   }
   //dismiss modal
   dismiss() {
